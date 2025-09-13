56d660a4888e3ee681ba5bb12c433fbe219d0991
Irina-C <iryna.vorotynska@gmail.com>
2025-09-13 15:34:38 +0200

    Changed styles.css and contact.js for contact page

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,5 @@
 // src/contact.js
+import phoneImage from "./assets/images/phone.svg";
 
 export default function loadContact() {
   const content = document.getElementById("content");
@@ -10,26 +11,38 @@ export default function loadContact() {
 
             <!-- Mail -->
           <section class="contact-us" id="mail">
+            <div class="contact-background">
             <h2 class="section-title">Contact Us</h2>
-            <form action="">
+            <form class="create-account" action="#">
               <div class="personal-data">
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="Enter a valid email address" required/>
-                <label for="name">Name</label>
-                <input type="text" id="name" name="name" placeholder="Enter your name" required
-                  title="Please enter a valid first name, example: Nic"/>
+                <div class="col">
+                  <label for="email">Email</label>
+                  <input type="email" id="email" name="email" placeholder="Enter a valid email address" required/>
+                </div>
+                <div class="col">
+                  <label for="name">Name</label>
+                  <input type="text" id="name" name="name" placeholder="Enter your name" required
+                         title="Please enter a valid name, example: Nic"/>
+                </div>
               </div>
-              <label for="message">Message
-              <textarea id="massage" placeholder="Enter you message"></textarea></label>
-              <input type="submit" value="Submit" class="btn-sabmit"/>
+              <div class="message">
+                <label for="message">Message</label>
+              <textarea id="message" rows="6" placeholder="Enter you message"></textarea>
+              </div>
+              <button type="submit" value="Submit" class="btn btn-submit">Submit</button>
             </form>
+            </div>
           </section>
           
             <!-- Info -->
           <section class="info">
-            <h2 class="section-title">Keep up to date with us</h2>
-            <input type="email" required placeholder="Enter a valid email address"/>
-            <input type="submit" value="Submit" class="btn-sabmit" />
+            <div class="contact-background">
+              <h2 class="section-title">Keep up to date with us</h2>
+              <form class="create-account" action="#">
+              <input type="email" required placeholder="Enter a valid email address"/>
+              <button type="submit" value="Submit" class="btn btn-submit">Submit</button>
+              </form>
+            </div>
           </section>
 
             <!-- Our contact -->
@@ -46,16 +59,17 @@ export default function loadContact() {
               </div>
 
               <div class="items">
-
                 <div class="row">
-                  <div class="contact-svg"></div>
+                  <div class="contact-svg">
+                     
+                  </div>
                   <div class="item">
                     <h3 class="item-title">Chart to us</h3>
                     <p class="item-text">Our friendly team is here to help.</p>
-                    <p class="item-text"><a href="mail">hi@ourcompany.com</a></p>
+                    <p class="item-text"><a href="#">hi@ourcompany.com</a></p>
                   </div>
                 </div>
-                  <div class="row">
+                <div class="row">
                     <div class="contact-svg"></div>
                     <div class="item">
                       <h3 class="item-title">Office</h3>
@@ -63,19 +77,19 @@ export default function loadContact() {
                       <p class="item-text">121 Rock Street, 21 Avenue</p>
                       <p class="item-text">New York, NY 92103-9000</p>
                     </div>
-                  </div>
-
-                  <div class="row">
+                </div>
+                <div class="row">
                     <div class="contact-svg"></div>
                     <div class="item">
                       <h3 class="item-title">Phone</h3>
                       <p class="item-text">Mon-Fri from 8am to 5am</p>
                       <p class="item-text">+1(555) 000-000</p>
                     </div>
-                  </div>
                 </div>
-            <div>
-        </section>
+              </div>
+
+            </div>
+          </section>
         </div>
     `;
   content.appendChild(contactDiv);

1057c1cd6a77e4568c78e9ba9c7338b640107b19
Irina-C <iryna.vorotynska@gmail.com>
2025-09-14 21:56:27 +0200

    Add about.js. Changet contact.js, styles.css. Improved typography.

---
+++ src/about.js
@@ -0,0 +1,12 @@
+// src/about.js
+export default function loadAbout() {
+    const content = document.getElementById("content");
+    const aboutDiv = document.createElement("div");
+
+    aboutDiv.innerHTML = `
+    <div class="container">
+      <h1>Hi, I am About</h1>
+    </div>     
+   `;
+    content.appendChild(aboutDiv);
+}

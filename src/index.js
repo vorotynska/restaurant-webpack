27a464160cfc930d42c1ff78a0167af4216014f4
Irina-C <iryna.vorotynska@gmail.com>
2025-09-22 16:48:16 +0200

    Changed index.js: the menu closes when you click on the button in the nav and when you click outside the menu or burger area.

--- src/index.js
+++ src/index.js
@@ -10,41 +10,61 @@ function clearContent() {
     content.innerHTML = "";
 }
 
+const nav = document.querySelector(".nav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+// функция закрытия меню
+function closeNav() {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+}
+
 document.getElementById("homeBtn").addEventListener("click", () => {
     clearContent();
     loadHome();
+    closeNav();
 });
 
 document.getElementById("menuBtn").addEventListener("click", () => {
     clearContent();
     loadMenu();
+    closeNav();
 });
 
 document.getElementById("aboutBtn").addEventListener("click", () => {
     clearContent();
     loadAbout();
+    closeNav();
 });
 
 document.getElementById("contactBtn").addEventListener("click", () => {
     clearContent();
     loadContact();
+    closeNav();
 });
-
-const nav = document.querySelector(".nav");
-const navToggle = document.querySelector(".mobile-nav-toggle");
 
 // click the hamburger button
 navToggle.addEventListener("click", () => {
     const visibility = nav.getAttribute("data-visible");
     if (visibility === "false") {
-        nav.setAttribute("data-visible", true)
+        nav.setAttribute("data-visible", true);
         navToggle.setAttribute("aria-expanded", true);
     } else {
-        nav.setAttribute("data-visible", false);
-        navToggle.setAttribute("aria-expanded", false);
+        closeNav();
     }
-})
+});
 
+// close when clicking outside the menu
+document.addEventListener("click", (e) => {
+    const isNavOpen = nav.getAttribute("data-visible") === "true";
+    if (
+        isNavOpen &&
+        !nav.contains(e.target) && // click not on the menu
+        !navToggle.contains(e.target) // and not for the burger
+    ) {
+        closeNav();
+    }
+});
 
 // load home page default
 loadHome();
